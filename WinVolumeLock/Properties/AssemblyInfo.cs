0320d0b8f8182c7cef4a990a1282c6bc8711c3b3
Brosilio <15786675+Brosilio@users.noreply.github.com>
2021-07-31 15:19:08 -0400

    change metadata

--- WinVolumeLock/Properties/AssemblyInfo.cs
+++ WinVolumeLock/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WinVolumeLock")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Lock or limit Windows volume levels")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinVolumeLock")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("Brosilio")]
+[assembly: AssemblyProduct("https://github.com/brosilio/winvolumelock")]
+[assembly: AssemblyCopyright("Copyright Brosilio ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
